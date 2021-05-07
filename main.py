e777b94218dc7c528e54bac6cce7490fa1013855
kerbalwzy <kerbalwzy@gmail.com>
2021-05-07 15:21:42 +0800

    Update: 更新前端UI基础代码

--- main.py
+++ main.py
@@ -1,54 +1,74 @@
-# -*- coding=utf-8 -*-
-# @Author: wzy
-# @Time: 2021/4/30
-"""
-1.打开一个wxPython+CEFPython的GUI窗口，并访问配置文件中的URL，进行网页渲染
-2.通过Subprocess开启Web服务的后端程序，并记录进程ID
-3.浏览器退出时，同时根据后端进程ID关闭所有的后端程序
-"""
+# Example of embedding CEF Python browser using wxPython library.
+# This example has a top menu and a browser widget without navigation bar.
+
+# Tested configurations:
+# - wxPython 4.0 on Windows/Mac/Linux
+# - wxPython 3.0 on Windows/Mac
+# - wxPython 2.8 on Linux
+# - CEF Python v66.0+
+
+import os
 import platform
 import sys
 
 import wx
 from cefpython3 import cefpython as cef
-# 常量
+
+# Platforms
 WINDOWS = (platform.system() == "Windows")
 LINUX = (platform.system() == "Linux")
 MAC = (platform.system() == "Darwin")
-# 运行前检查环境
-print("CEF Python {ver}".format(ver=cef.__version__))
-print("Python {ver} {arch}".format(ver=platform.python_version(), arch=platform.architecture()[0]))
-print("wxPython {ver}".format(ver=wx.version()))
-assert cef.__version__ >= "66.0", "要求使用CEF Python v66.0+的版本"
-try:
-    cef.GetVersion()
-except Exception as e:
-    raise Exception("{err}\nPython解释器版本在当前操作系统上不支持CEFPython66.0，请重新选择解释器".format(err=e))
+
 if MAC:
     try:
         # noinspection PyUnresolvedReferences
         from AppKit import NSApp
-        # Make the content view for the window have a layer.
-        # This will make all sub-views have layers. This is
-        # necessary to ensure correct layer ordering of all
-        # child views and their layers. This fixes Window
-        # glitchiness during initial loading on Mac (Issue #371).
-        NSApp.windows()[0].contentView().setWantsLayer_(True)
     except ImportError:
-        raise Exception("Error: PyObjC package is missing, cannot fix Issue #371\n"
-                        "To install PyObjC type: pip install -U pyobjc")
-if WINDOWS:
-    # noinspection PyUnresolvedReferences, PyArgumentList
-    cef.DpiAware.EnableHighDpiSupport()
-# 替换异常处理逻辑，保证异常发生时能够结束所有进程
-sys.excepthook = cef.ExceptHook
+        print("[wxpython.py] Error: PyObjC package is missing, "
+              "cannot fix Issue #371")
+        print("[wxpython.py] To install PyObjC type: "
+              "pip install -U pyobjc")
+        sys.exit(1)
 
-# 配置常量
+# Configuration
 WIDTH = 1000
 HEIGHT = 618
+INDEX_URL = "https://www.baidu.com"
+TITLE = "SinRiu效率工具"
 
-# 全局变量
-GCountWindows = 0
+# Globals
+g_count_windows = 0
+
+
+def main():
+    check_versions()
+    sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
+    settings = {}
+    if MAC:
+        # Issue #442 requires enabling message pump on Mac
+        # and calling message loop work in a timer both at
+        # the same time. This is an incorrect approach
+        # and only a temporary fix.
+        settings["external_message_pump"] = True
+    if WINDOWS:
+        # noinspection PyUnresolvedReferences, PyArgumentList
+        cef.DpiAware.EnableHighDpiSupport()
+    cef.Initialize(settings=settings)
+    app = CefApp(False)
+    app.MainLoop()
+    del app  # Must destroy before calling Shutdown
+    if not MAC:
+        # On Mac shutdown is called in OnClose
+        cef.Shutdown()
+
+
+def check_versions():
+    print("[wxpython.py] CEF Python {ver}".format(ver=cef.__version__))
+    print("[wxpython.py] Python {ver} {arch}".format(
+        ver=platform.python_version(), arch=platform.architecture()[0]))
+    print("[wxpython.py] wxPython {ver}".format(ver=wx.version()))
+    # CEF Python version requirement
+    assert cef.__version__ >= "66.0", "CEF Python v66.0+ required to run this"
 
 
 def scale_window_size_for_high_dpi(width, height):
@@ -70,41 +90,206 @@ def scale_window_size_for_high_dpi(width, height):
 
 class MainFrame(wx.Frame):
 
-    def __init__(self, parent=None, id=None, title=None, pos=None, size=None, style=None, name=None):
+    def __init__(self):
         self.browser = None
+
         # Must ignore X11 errors like 'BadWindow' and others by
         # installing X11 error handlers. This must be done after
-        # wx was initialized.
+        # wx was intialized.
         if LINUX:
             cef.WindowUtils.InstallX11ErrorHandlers()
-        size = size or (WIDTH, HEIGHT)
-        size = scale_window_size_for_high_dpi(*size)
-        super().__init__(parent, id, title, pos, size, style, name)
 
-        global GCountWindows
-        GCountWindows += 1
+        global g_count_windows
+        g_count_windows += 1
+
+        if WINDOWS:
+            # noinspection PyUnresolvedReferences, PyArgumentList
+            print("[wxpython.py] System DPI settings: %s"
+                  % str(cef.DpiAware.GetSystemDpi()))
+        if hasattr(wx, "GetDisplayPPI"):
+            print("[wxpython.py] wx.GetDisplayPPI = %s" % wx.GetDisplayPPI())
+        print("[wxpython.py] wx.GetDisplaySize = %s" % wx.GetDisplaySize())
+
+        print("[wxpython.py] MainFrame declared size: %s"
+              % str((WIDTH, HEIGHT)))
+        size = scale_window_size_for_high_dpi(WIDTH, HEIGHT)
+        print("[wxpython.py] MainFrame DPI scaled size: %s" % str(size))
+
+        wx.Frame.__init__(self, parent=None, id=wx.ID_ANY, title=TITLE, size=(WIDTH, HEIGHT),
+                          style=wx.DEFAULT_FRAME_STYLE ^ wx.MAXIMIZE_BOX ^ wx.RESIZE_BORDER)
+        # wxPython will set a smaller size when it is bigger
+        # than desktop size.
+        print("[wxpython.py] MainFrame actual size: %s" % self.GetSize())
+
+        self.setup_icon()
+        # self.create_menu()
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+
+        # Set wx.WANTS_CHARS style for the keyboard to work.
+        # This style also needs to be set for all parent controls.
+        self.browser_panel = wx.Panel(self, style=wx.WANTS_CHARS)
+        self.browser_panel.Bind(wx.EVT_SET_FOCUS, self.OnSetFocus)
+        self.browser_panel.Bind(wx.EVT_SIZE, self.OnSize)
+
+        if MAC:
+            # Make the content view for the window have a layer.
+            # This will make all sub-views have layers. This is
+            # necessary to ensure correct layer ordering of all
+            # child views and their layers. This fixes Window
+            # glitchiness during initial loading on Mac (Issue #371).
+            NSApp.windows()[0].contentView().setWantsLayer_(True)
+
+        if LINUX:
+            # On Linux must show before embedding browser, so that handle
+            # is available (Issue #347).
+            self.Show()
+            # In wxPython 3.0 and wxPython 4.0 on Linux handle is
+            # still not yet available, so must delay embedding browser
+            # (Issue #349).
+            if wx.version().startswith("3.") or wx.version().startswith("4."):
+                wx.CallLater(100, self.embed_browser)
+            else:
+                # This works fine in wxPython 2.8 on Linux
+                self.embed_browser()
+        else:
+            self.embed_browser()
+            self.Center()
+            self.Show()
+
+    def setup_icon(self):
+        icon_file = os.path.join(os.getcwd(), "favicon.png")
+        # wx.IconFromBitmap is not available on Linux in wxPython 3.0/4.0
+        if os.path.exists(icon_file) and hasattr(wx, "IconFromBitmap"):
+            icon = wx.IconFromBitmap(wx.Bitmap(icon_file, wx.BITMAP_TYPE_PNG))
+        else:
+            icon = wx.Icon(icon_file, wx.BITMAP_TYPE_ICO)
+        self.SetIcon(icon)
+
+    def create_menu(self):
+        filemenu = wx.Menu()
+        filemenu.Append(1, "Some option")
+        filemenu.Append(2, "Another option")
+        menubar = wx.MenuBar()
+        menubar.Append(filemenu, "&File")
+        self.SetMenuBar(menubar)
+
+    def embed_browser(self):
+        window_info = cef.WindowInfo()
+        (width, height) = self.browser_panel.GetClientSize().Get()
+        assert self.browser_panel.GetHandle(), "Window handle not available"
+        window_info.SetAsChild(self.browser_panel.GetHandle(),
+                               [0, 0, width, height])
+        self.browser = cef.CreateBrowserSync(window_info, url=INDEX_URL)
+        self.browser.SetClientHandler(FocusHandler())
+
+    def OnSetFocus(self, _):
+        if not self.browser:
+            return
+        if WINDOWS:
+            cef.WindowUtils.OnSetFocus(self.browser_panel.GetHandle(),
+                                       0, 0, 0)
+        self.browser.SetFocus(True)
+
+    def OnSize(self, _):
+        if not self.browser:
+            return
+        if WINDOWS:
+            cef.WindowUtils.OnSize(self.browser_panel.GetHandle(),
+                                   0, 0, 0)
+        elif LINUX:
+            (x, y) = (0, 0)
+            (width, height) = self.browser_panel.GetSize().Get()
+            self.browser.SetBounds(x, y, width, height)
+        self.browser.NotifyMoveOrResizeStarted()
+
+    def OnClose(self, event):
+        print("[wxpython.py] OnClose called")
+        if not self.browser:
+            # May already be closing, may be called multiple times on Mac
+            return
+
+        if MAC:
+            # On Mac things work differently, other steps are required
+            self.browser.CloseBrowser()
+            self.clear_browser_references()
+            self.Destroy()
+            global g_count_windows
+            g_count_windows -= 1
+            if g_count_windows == 0:
+                cef.Shutdown()
+                wx.GetApp().ExitMainLoop()
+                # Call _exit otherwise app exits with code 255 (Issue #162).
+                # noinspection PyProtectedMember
+                os._exit(0)
+        else:
+            # Calling browser.CloseBrowser() and/or self.Destroy()
+            # in OnClose may cause app crash on some paltforms in
+            # some use cases, details in Issue #107.
+            self.browser.ParentWindowWillClose()
+            event.Skip()
+            self.clear_browser_references()
+
+    def clear_browser_references(self):
+        # Clear browser references that you keep anywhere in your
+        # code. All references must be cleared for CEF to shutdown cleanly.
+        self.browser = None
 
 
-class UIMain:
-    def __init__(self, index_url: str):
-        self.index_url = index_url
-        self.cef = cef
-        pass
+class FocusHandler(object):
+    def OnGotFocus(self, browser, **_):
+        # Temporary fix for focus issues on Linux (Issue #284).
+        if LINUX:
+            print("[wxpython.py] FocusHandler.OnGotFocus:"
+                  " keyboard focus fix (Issue #284)")
+            browser.SetFocus(True)
 
-    def init_window(self):
-        self.cef.Initialize()
-        self.cef.CreateBrowserSync(url=self.index_url)
-        self.cef.MessageLoop()
 
-    def close_window(self):
-        self.cef.Shutdown()
+class CefApp(wx.App):
 
-    def run(self):
-        self.init_window()
-        self.close_window()
+    def __init__(self, redirect):
+        self.timer = None
+        self.timer_id = 1
+        self.is_initialized = False
+        super(CefApp, self).__init__(redirect=redirect)
 
-        pass
+    def OnPreInit(self):
+        super(CefApp, self).OnPreInit()
+        # On Mac with wxPython 4.0 the OnInit() event never gets
+        # called. Doing wx window creation in OnPreInit() seems to
+        # resolve the problem (Issue #350).
+        if MAC and wx.version().startswith("4."):
+            print("[wxpython.py] OnPreInit: initialize here"
+                  " (wxPython 4.0 fix)")
+            self.initialize()
+
+    def OnInit(self):
+        self.initialize()
+        return True
+
+    def initialize(self):
+        if self.is_initialized:
+            return
+        self.is_initialized = True
+        self.create_timer()
+        frame = MainFrame()
+        self.SetTopWindow(frame)
+        frame.Show()
+
+    def create_timer(self):
+        # See also "Making a render loop":
+        # http://wiki.wxwidgets.org/Making_a_render_loop
+        # Another way would be to use EVT_IDLE in MainFrame.
+        self.timer = wx.Timer(self, self.timer_id)
+        self.Bind(wx.EVT_TIMER, self.on_timer, self.timer)
+        self.timer.Start(10)  # 10ms timer
+
+    def on_timer(self, _):
+        cef.MessageLoopWork()
+
+    def OnExit(self):
+        self.timer.Stop()
+        return 0
 
 
 if __name__ == '__main__':
-    UIMain(index_url="https://www.baidu.com").run()
+    main()
